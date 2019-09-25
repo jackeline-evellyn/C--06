2eadaaf84fd16f544930f47517e16fec57c796a7
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2019-09-25 13:53:04 -0300

    Atualização

---
+++ 5-ModificadoresParametroRefOut/5-ModificadoresParametroRefOut/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace _5_ModificadoresParametroRefOut
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+
+    }
+}
---
+++ 5-ModificadoresParametroRefOut/5-ModificadoresParametroRefOut/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_ModificadoresParametroRefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
