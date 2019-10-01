164a3e3efc1a977ae2ebbd512e4ab2ce36b87442
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2019-10-01 17:09:14 -0300

    Atualização

---
+++ 3-Vetores2/3-Vetores2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_Vetores2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
